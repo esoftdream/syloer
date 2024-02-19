958ecfec0b3c15c1bf16f0ce2422ff32d7e78c1e
Toto <totoprayogo1916@gmail.com>
2024-02-19 21:39:15 +0700

    update Config

---
+++ src/Config/Service.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Esoftdream\Syloer\Config;
+
+use Config\Services as BaseService;
+use Esoftdream\Syloer\Libraries\Logger;
+
+class Services extends BaseService
+{
+    public static function logger(bool $getShared = true)
+    {
+        if ($getShared) {
+            return static::getSharedInstance('logger');
+        }
+
+        return new Logger(config('App'));
+    }
+}
